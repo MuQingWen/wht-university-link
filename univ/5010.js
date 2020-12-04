f3f00c6170116dd3010a526894d9695c7a59cffe
MuQingWen <61997336+MuQingWen@users.noreply.github.com>
2020-12-04 09:52:15 +0800

    Create 5010.js

---
+++ univ/5010.js
@@ -0,0 +1,20 @@
+var univ_list = {
+    school: "大连民族大学",
+    id: "5010",
+    link: [ {
+        name: "民大主页",
+        url: "https://www.dlnu.edu.cn/hhh/index.htm"
+    }, {
+        name: "信息门户",
+        url: "http://ehall.dlnu.edu.cn/new/index.html"
+    }, {
+        name: "图书馆",
+        url: "https://lib.dlnu.edu.cn/"
+    }, {
+        name: "校园网",
+        url: "http://210.30.0.114/sam/"
+    }, {
+        name: "VPN",
+        url: "https://www.dlnu.edu.cn/hhh/vpn/"
+    } ]
+};
